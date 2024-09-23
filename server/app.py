eb10b091072413ee9d48ef1e1a958d2eb7503b6b
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-09-23 12:07:54 -0600

    login signup

--- server/app.py
+++ server/app.py
@@ -8,13 +8,42 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
-
+    def post(self):
+        print('post1')
+        try:
+            new_user = User(
+                username=request.json['username'],
+                image_url=request.get_json()['image_url'] if request.get_json()['image_url'] else None,
+                bio = request.get_json()['bio'] if request.get_json()['bio'] else None,
+            )
+            print (new_user)
+            new_user.password_hash = request.get_json()['password']
+            db.session.add(new_user)
+            db.session.commit()
+            session['user_id']=new_user.id
+            return new_user.to_dict(), 201
+        except:
+            return {'error': '422 Unprocessable Entity'}, 422
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session.get('user_id'):
+            return User.query.get(session['user_id']).to_dict()
+        else:
+            return {'error': '401 Unauthorized'}, 401
 
 class Login(Resource):
-    pass
+    def post(self):
+        print('post2')
+        user = User.query.filter(
+            User.username == request.get_json()['username']
+        ).first()
+        if user and user.authenticate(request.get_json()['password']):
+            session['user_id'] = user.id
+            print(session['user_id'])
+
+            return user.to_dict()
+        else:
+            return {'error': '401 Unauthorized'}, 401
 
 class Logout(Resource):
     pass
