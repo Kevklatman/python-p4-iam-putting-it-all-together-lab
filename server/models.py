271086f76ddebbd31ba02489d348bc61348e2ce5
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-09-22 19:10:15 -0600

    added bcrypt

--- server/models.py
+++ server/models.py
@@ -5,16 +5,34 @@ from sqlalchemy_serializer import SerializerMixin
 from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
-
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    _password_hash= db.Column(db.String)
-    image_url= db.Column(db.String)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
     bio = db.Column(db.String)
 
     recipes = db.relationship('Recipe', back_populates='user')
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('password_hash is not a readable attribute')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password)
+
+    @validates('username')
+    def validate_username(self, key, username):
+        if not username:
+            raise ValueError('Username must be present')
+        return username
+
+    serialize_rules = ('-_password_hash',)
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
