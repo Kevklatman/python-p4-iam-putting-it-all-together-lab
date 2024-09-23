f9f2c5b971c6ec1e087b195f11d4bb8e22ea023a
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-09-22 19:08:36 -0600

    added tables

--- server/models.py
+++ server/models.py
@@ -5,11 +5,24 @@ from sqlalchemy_serializer import SerializerMixin
 from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
+
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    _password_hash= db.Column(db.String)
+    image_url= db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship('Recipe', back_populates='user')
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
     
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    instructions = db.Column(db.String)
+    minutes_to_complete = db.Column(db.String)
+    user_id = db.Column(db.ForeignKey('users.id'), index=True)
+
+    user = db.relationship('User', back_populates='recipes')
